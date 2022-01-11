d3f1c02c5aa7bb3a06c36fc136a6333f0f83d5ef
twangrt <t@vaara.se>
2022-01-11 19:44:26 +0100

    Update

--- elchapo.py
+++ elchapo.py
@@ -1,12 +1,22 @@
 import serial
-from time import sleep #Kan vara nödvändigt
+from time import sleep 
 import time, datetime, csv
-import keyboard #Det här är dumt, men det enklaste jag hittade. *nix system måste köra som Root, vilket inte är önskvärt.
+import keyboard     #Det här är dumt, men det enklaste jag hittade. *nix system måste köra som Root, vilket inte är önskvärt.
 
 usb_serialport = 'COM4'
-time_add = 5 #Testa, måste tweakas under riktiga förhållanden för att inte evaluate_data funktionen ska gå sönder.
-sleep_time = 0 #Detta borde vara samma värde som arduino delay.
+
+time_add = 5        #Testa, måste tweakas under riktiga förhållanden.
+sleep_time = 0      #Detta borde vara samma värde som arduino delay.
 timeout = 0
+
+#Värden för datainsamling
+x_trigger_min = -0.2
+x_trigger_max = 0.2
+y_trigger_min = -0.2
+y_trigger_max = 0.2
+z_trigger_min = 9.0
+z_trigger_max = 11.0
+
 
 ser = serial.Serial(usb_serialport) #Öppna serialporten
 
@@ -24,13 +34,13 @@ def evaluate_data(line): #Kontrollerar brytvärde för datainsamling.
 
     record_data = False
 
-    if line[0] > 2:
+    if not x_trigger_min < line[0] < x_trigger_max:
         record_data = True
 
-    if line[1] > 2:
+    if not y_trigger_min < line[1] < y_trigger_max:
         record_data = True
 
-    if line[2] > 11:
+    if not z_trigger_min < line[2] < z_trigger_max:
         record_data = True
         
     return record_data
